d13a30898b8956ee7d8aff7963fabecbbf2353ce
Naveendgp <naveendgp@gmail.com>
2024-08-18 17:37:51 +0530

    Map

---
+++ Client/src/Components/Map/Map.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './map.css'
+
+
+export const Map = () => {
+  return (
+    <div className='Map-container'>Map</div>
+  )
+}
--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { Map } from '../Components/Map/Map'
 const Home = () => {
   return (
     <div className='Home'>
-        <h1>Home</h1>
+
+
+        <h1 className='homeHead'>Home</h1>
+
+        <div className='Map-grid'>
+          <Map/>
+        </div>
     </div>
   )
 }
